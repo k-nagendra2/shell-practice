30db994cefc76e1ad1984fe91df33f621335e031
nagendra <new.nagendra25@gmail.com>
2025-05-27 19:38:14 +0530

    hello world stared

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: hi,hru?"
+echo "musk: doing good,HrU"
+echo " Trump: im good too , whats up"
+echo " musk : nothing much Trump ,im going"
+echo " Trump : ok good to musk"
